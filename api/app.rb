1975406f66dd3deeff33e1c74f7186b56bb5de94
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2024-06-11 09:32:27 -0300

    remove comment about database

--- api/app.rb
+++ api/app.rb
@@ -4,14 +4,6 @@ require 'sinatra'
 require 'sinatra/activerecord'
 # require './models'
 
-# set :database, {
-#   adapter: 'postgresql',
-#   database: 'ourmaps_development',
-#   host: 'db',
-#   username: 'postgres',
-#   password: 'password'
-# }
-
 set :bind, '0.0.0.0'
 
 get '/' do
